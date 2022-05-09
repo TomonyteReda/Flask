530e0731036ca5225aacf02fb99aae40ca653821
TomonyteReda <reda.tomonyte@gmail.com>
2022-05-09 20:47:55 +0300

    code updated with password reset functions and error html

--- Flask_User_Registration_task/app.py
+++ Flask_User_Registration_task/app.py
@@ -1,15 +1,23 @@
 import os
 from flask import Flask, render_template, redirect, url_for, flash, request, g
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import DateTime
+from datetime import datetime
 from flask_bcrypt import Bcrypt
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from flask_login import LoginManager, current_user, logout_user, login_user, UserMixin, login_required
 import forms
+import secrets
+from PIL import Image
+from flask_mail import Message, Mail
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4654f5dfadsrfasdr54e6rae'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'biudzetas.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'new_budget.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -19,22 +27,66 @@ login_manager.login_view = 'prisijungti'
 login_manager.login_message_category = 'info'
 
 
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'reda.testpython@gmail.com'
+app.config['MAIL_PASSWORD'] = 'testpy123'
+mail = Mail(app)
+
 class Vartotojas(db.Model, UserMixin):
     __tablename__ = "vartotojas"
     id = db.Column(db.Integer, primary_key=True)
     vardas = db.Column("Vardas", db.String(20), unique=True, nullable=False)
     el_pastas = db.Column("El. pašto adresas", db.String(120), unique=True, nullable=False)
+    nuotrauka = db.Column(db.String(20), nullable=False, default='default.jpg')
     slaptazodis = db.Column("Slaptažodis", db.String(60), unique=True, nullable=False)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return Vartotojas.query.get(user_id)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Irasas(db.Model):
+    __tablename__ = "irasas"
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column("Data", DateTime, default=datetime.now())
+    suma = db.Column("Vardas", db.Integer)
+    vartotojas_id = db.Column(db.Integer, db.ForeignKey("vartotojas.id"))
+    vartotojas = db.relationship("Vartotojas", lazy=True)
+
+
+class ManoModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.el_pastas == "reda.tomonyte@gmail.com"
+
+
+admin = Admin(app)
+admin.add_view(ManoModelView(Vartotojas, db.session))
+admin.add_view(ModelView(Irasas, db.session))
 
 
 @login_manager.user_loader
 def load_user(vartotojo_id):
+    db.create_all()
     return Vartotojas.query.get(int(vartotojo_id))
 
 
 @app.route("/registruotis", methods=['GET', 'POST'])
 def registruotis():
-    db.create_all()
+    # db.create_all()
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = forms.RegistracijosForma()
@@ -71,16 +123,141 @@ def atsijungti():
     return redirect(url_for('index'))
 
 
-@app.route("/paskyra")
+@app.route("/admin")
 @login_required
-def account():
-    return render_template('paskyra.html', title='Paskyra')
+def admin():
+    return redirect(url_for(admin))
+
+
+@app.route("/irasai")
+@login_required
+def records():
+    db.create_all()
+    page = request.args.get('page', 1, type=int)
+    visi_irasai = Irasas.query.filter_by(vartotojas_id=current_user.id).order_by(Irasas.data.desc()).paginate(page=page,per_page=5)
+    return render_template("irasai.html", visi_irasai=visi_irasai, datetime=datetime)
+
+
+@app.route("/naujas_irasas", methods=["GET", "POST"])
+def new_record():
+    # db.create_all()
+    forma = forms.IrasasForm()
+    if forma.validate_on_submit():
+        naujas_irasas = Irasas(suma=forma.suma.data, vartotojas_id=current_user.id)
+        db.session.add(naujas_irasas)
+        db.session.commit()
+        flash(f"Įrašas sukurtas", 'success')
+        return redirect(url_for('records'))
+    return render_template("prideti_irasa.html", form=forma)
+
+
+@app.route("/balansas")
+def balance():
+    try:
+        visi_irasai = Irasas.query.filter_by(vartotojas_id=current_user.id)
+    except:
+        visi_irasai = []
+    balansas = 0
+    for irasas in visi_irasai:
+        balansas += irasas.suma
+    return render_template("balansas.html", balansas=balansas)
 
 
 @app.route("/timetable")
 @login_required
 def timetable():
     return render_template('timetable.html', title='Timetable')
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profilio_nuotraukos', picture_fn)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+
+@app.route("/paskyra", methods=['GET', 'POST'])
+@login_required
+def paskyra():
+    form = forms.PaskyrosAtnaujinimoForma()
+    if form.validate_on_submit():
+        if form.nuotrauka.data:
+            nuotrauka = save_picture(form.nuotrauka.data)
+            current_user.nuotrauka = nuotrauka
+        current_user.vardas = form.vardas.data
+        current_user.el_pastas = form.el_pastas.data
+        db.session.commit()
+        flash('Tavo paskyra atnaujinta!', 'success')
+        return redirect(url_for('paskyra'))
+    elif request.method == 'GET':
+        form.vardas.data = current_user.vardas
+        form.el_pastas.data = current_user.el_pastas
+    nuotrauka = url_for('static', filename='profilio_nuotraukos/' + current_user.nuotrauka)
+    return render_template('paskyra.html', title='Account', form=form, nuotrauka=nuotrauka)
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Slaptažodžio atnaujinimo užklausa',
+                  sender='reda.testpython@gmail.com',
+                  recipients=[user.el_pastas])
+    msg.body = f'''Norėdami atnaujinti slaptažodį, paspauskite nuorodą:
+    {url_for('reset_token', token=token, _external=True)}
+    Jei jūs nedarėte šios užklausos, nieko nedarykite ir slaptažodis nebus pakeistas.
+    '''
+    print(msg.body)
+    mail.send(msg)
+
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = forms.UzklausosAtnaujinimoForma()
+    if form.validate_on_submit():
+        user = Vartotojas.query.filter_by(el_pastas=form.el_pastas.data).first()
+        send_reset_email(user)
+        flash('Jums išsiųstas el. laiškas su slaptažodžio atnaujinimo instrukcijomis.', 'info')
+        return redirect(url_for('prisijungti'))
+    return render_template('reset_request.html', title='Reset Password', form=form)
+
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = Vartotojas.verify_reset_token(token)
+    if user is None:
+        flash('Užklausa netinkama arba pasibaigusio galiojimo', 'warning')
+        return redirect(url_for('reset_request'))
+    form = forms.SlaptazodzioAtnaujinimoForma()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.slaptazodis.data).decode('utf-8')
+        user.slaptazodis = hashed_password
+        db.session.commit()
+        flash('Tavo slaptažodis buvo atnaujintas! Gali prisijungti', 'success')
+        return redirect(url_for('prisijungti'))
+    return render_template('reset_token.html', title='Reset Password', form=form)
+
+
+@app.errorhandler(404)
+def klaida_404(error):
+    return render_template("404.html", original=error), 404
+
+
+@app.errorhandler(403)
+def klaida_403(error):
+    return render_template("403.html", original=error), 403
+
+
+@app.errorhandler(500)
+def klaida_500(error):
+    return render_template("500.html", original=error), 500
 
 
 @app.route("/")
--- Flask_User_Registration_task/forms.py
+++ Flask_User_Registration_task/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, BooleanField, StringField, PasswordField
-from wtforms.validators import DataRequired, ValidationError, EqualTo
+from wtforms import SubmitField, BooleanField, StringField, PasswordField, FloatField
+from wtforms.validators import DataRequired, ValidationError, EqualTo, Email
+from flask_wtf.file import FileField, FileAllowed
+from app import Vartotojas
 import app
 
 
@@ -27,3 +29,43 @@ class PrisijungimoForma(FlaskForm):
     slaptazodis = PasswordField('Slaptažodis', [DataRequired()])
     prisiminti = BooleanField("Prisiminti mane")
     submit = SubmitField('Prisijungti')
+
+
+class IrasasForm(FlaskForm):
+    suma = FloatField('Suma', [DataRequired()])
+    submit = SubmitField('Įvesti')
+
+
+class PaskyrosAtnaujinimoForma(FlaskForm):
+    vardas = StringField('Vardas', [DataRequired()])
+    el_pastas = StringField('El. paštas', [DataRequired()])
+    nuotrauka = FileField('Atnaujinti profilio nuotrauką', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Atnaujinti')
+
+    def tikrinti_varda(self, vardas):
+        if vardas.data != app.current_user.vardas:
+            vartotojas = app.Vartotojas.query.filter_by(vardas=vardas.data).first()
+            if vartotojas:
+                raise ValidationError('Šis vardas panaudotas. Pasirinkite kitą.')
+
+    def tikrinti_pasta(self, el_pastas):
+        if el_pastas.data != app.current_user.el_pastas:
+            vartotojas = app.Vartotojas.query.filter_by(el_pastas=el_pastas.data).first()
+            if vartotojas:
+                raise ValidationError('Šis el. pašto adresas panaudotas. Pasirinkite kitą.')
+
+
+class UzklausosAtnaujinimoForma(FlaskForm):
+    el_pastas = StringField('El. paštas', validators=[DataRequired(), Email()])
+    submit = SubmitField('Gauti')
+
+    def validate_email(self, el_pastas):
+        user = Vartotojas.query.filter_by(el_pastas=el_pastas.data).first()
+        if user is None:
+            raise ValidationError('password will be sent to your email')
+
+
+class SlaptazodzioAtnaujinimoForma(FlaskForm):
+    slaptazodis = PasswordField('Slaptažodis', validators=[DataRequired()])
+    patvirtintas_slaptazodis = PasswordField('Pakartokite slaptažodį', validators=[DataRequired(), EqualTo('slaptazodis')])
+    submit = SubmitField('Atnaujinti Slaptažodį')
